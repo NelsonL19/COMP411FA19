27b3d8bb18c4319759641b423a9545037e73318e
Nelson Lopez <nelson19@live.unc.edu>
2019-10-29 02:07:22 -0400

    Lab7.c progress
    Need to complete CC next

--- Lab 7/lab7.c
+++ Lab 7/lab7.c
@@ -4,6 +4,7 @@ int AA[100];  		// linearized version of A[10][10]
 int BB[100];  		// linearized version of B[10][10]
 int CC[100];  		// linearized version of C[10][10]
 int m;       		// actual size of the above matrices is mxm, where m is at most 10
+int product; 
 
 
 /* ------- Function proto-types put here  ------------------
@@ -27,81 +28,71 @@ int main() {
 
 	// TODO: add code here
 
-int userInputsWidthA[10];
-int userInputsLengthA[10]; 
-int userInputsWidthB[10]; 
-int userInputsLengthB[10]; 
-
-
 printf("%s\n", "Hello and welcome to the Matrix!");
 
 while (m <= 0 || m > 10) {
 	printf("%s\n", "Give me an integer to represent the size of each matrices, from 1 to 10");
+	printf("%s", "m = ");
 	scanf("%d", &m);
 }
 
 
-printf("%s\n", "Give me values to represent the data in the first matrix rows elements");
+int passA;
+int passB;
 
-for (int i = 0; i < m; i++) {
-	scanf("%d", &userInputsWidthA[i]);
-} 
+for (int i = 0; i < m*m; i++){
+	printf("%s", "AA[" );
+	printf("%d", i);
+	printf("%s", "] = ");
+	AA[i] = scanf("%d", &passA);
 
-printf("%s\n", "Give me values to represent the data in the first matrix columns elements");
-
-for (int i = 0; i < m; i++) {
-	scanf("%d", &userInputsLengthA[i]);
-} 
-
-//AA Creation
-
-for (int i = 0; i < m; i++) {
-	AA[i] = userInputsWidthA[i];
+	printf("%s", "BB[" );
+	printf("%d", i);
+	printf("%s", "] = ");
+	BB[i] = scanf("%d", &passB);
 }
 
-for (int i = 0; i < m; i++) {
-	AA[m+i] = userInputsLengthA[i];
+if (m == 1) {
+	printf("\n");
+	CC[0] = passA * passB;
+	printf("%s", "CC[" );
+	printf("%d", 0);
+	printf("%s", "] = ");
+	printf("%d", CC[0]);
+
+		printf("\n");
+		return(0);
 }
 
 
-for (int i = 0; i < m*m; i++) {
-	printf("%d", AA[i]);
 
-	if (i % m != 0) {
-		printf("\n"); 
+
+
+
+
+for (int i = 0; i < m-1; i++) {
+	for (int j = 0; j < m-1; j++) {
+		for (int k = 0; k < m; k++) {
+			product = product +(AA[i] * BB[k]);
+		}
+		CC[i] = CC[i] + AA[i+j] * BB[i+j+1];
 	}
-
-	printf(" "); 
+	CC[i] = product;	
 }
 
-printf("%s\n", "Give me values to represent the data in the second matrix rows elements");
+printf("\n");
 
-for (int i = 0; i < m; i++) {
-	scanf("%d", &userInputsWidthB[i]);
-} 
+for (int i = 0; i < m*m; i++){
+	printf("%s", "CC[" );
+	printf("%d", i);
+	printf("%s", "] = ");
+	printf("%d", CC[i]);
+	printf("\n");
 
-printf("%s\n", "Give me values to represent the data in the second matrix columns elements");
 
-for (int i = 0; i < m; i++) {
-	scanf("%d", &userInputsLengthB[i]);
-} 
-
-//BB Creation
-
-for (int i = 0; i < m; i++) {
-	BB[i] = userInputsWidthB[i];
-}
-
-for (int i = 0; i < m; i++) {
-	BB[m+i] = userInputsLengthB[i];
 }
 
 
 
-
-
-
-
 }
-
 /* ------- Function definitions below ------------------ */
