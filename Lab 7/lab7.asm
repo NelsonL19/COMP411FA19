4d3aa1ef4e6e17415e95cbd5f3541e6e06b4137a
Nelson Lopez <nelson19@live.unc.edu>
2019-10-30 01:23:34 -0400

    A7 Progress
    Oh Boy

--- Lab 7/lab7.asm
+++ Lab 7/lab7.asm
@@ -4,12 +4,18 @@
   CC:     .space 400  		# int CC[100]
   m:      .space 4   		# m is an int whose value is at most 10
                      		# actual size of the above matrices is mxm
+   
 
       # You may add more variables here if you need to
+.data 0x0
+  product: .space 400
+  mPrompt:	.asciiz "Give me an integer to represent the size of each matrices, from 1 to 10:\n"
+  aaPrompt:	.asciiz "Enter AA Values:\n"
+  bbPrompt:	.asciiz "Enter BB Values:\n"
+  ccPrompt:	.asciiz "CC Values are:\n"
+  newline:	.asciiz "\n"
 
-
-
-.text
+.text 0x3000
 
 main:
 
@@ -28,6 +34,33 @@ main:
 #  5.  Exit.
 #
 #----------------------------------------------
+
+  addi 	$v0, $0, 4  			# system call 4 is for printing a string
+  la 	$a0, mPrompt 			# address of mPrompt is in $a0
+  syscall           			# print the string
+
+  addi	$v0, $0, 5			# system call 5 is for reading an integer
+  syscall 				# integer value read is in $v0
+  add	$8, $0, $v0			# copy the width into $8
+
+ addi 	$v0, $0, 4  			# system call 4 is for printing a string
+  la 	$a0, aaPrompt 			# address of mPrompt is in $a0
+  syscall           			# print the string
+  
+  
+ ALoop:
+ 
+ 
+ 
+ BLoop:
+ 
+ MultiLoopi:
+  
+ MultiLoopj:
+ 
+ MultiLoopk: 
+
+ CLoop:
 
 
 exit:                     # this is code to terminate the program -- don't mess with this!
